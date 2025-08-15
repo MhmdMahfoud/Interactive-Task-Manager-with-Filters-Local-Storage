6e989b21652cb28fa6634c144ee2dbc21a81efa4
Mhmd Mahfoud <engmohammedmahfoud2001@gmail.com>
2025-08-15 10:27:25 +0300

    Error Message Display

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const taskForm = document.querySelector(".task-form");
 const taskInput = document.querySelector(".task-input");
+const taskList = document.querySelector(".task-list"); 
+const errorMsg = document.querySelector(".error-msg");   
